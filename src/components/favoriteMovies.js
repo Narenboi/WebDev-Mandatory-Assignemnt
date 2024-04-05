738023dc0a52517afb6315f6dd5aff89841b90b4
Narenboi <Luhavenkvist@yahoo.dk>
2024-04-05 22:32:53 +0200

    Favorites siden virker nu fuldt ud efter hensigtens, måden moviecard blev brugt på ændret en lille smule i favoriteMovies.js for at få den til at vise diverse movies i cards korrekt

--- src/components/favoriteMovies.js
+++ src/components/favoriteMovies.js
@@ -1,25 +1,20 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { useFetchFavoritesMoviesQuery } from '../store/apis/favoritesMoviesApi';
-import MovieCard from './movieCard'; // Assuming MovieCard component is in a file named 'MovieCard.js'
+import MovieCard from './movieCard';
 
 function FavoriteMovies() {
   const { data: favoriteMovies, error, isLoading, refetch } = useFetchFavoritesMoviesQuery();
-
   useEffect(() => {
-    refetch(); // Fetch favorite movies data when the component is rendered
-  }, [refetch]);
-
-  console.log('Favorite Movies:', favoriteMovies); // Log the favorite movies data
-
+    refetch();
+  }, []);
   return (
-    <div>
-      <h2>Favorite Movies</h2>
+    <div className="container">
+      <h2 className="mt-4 mb-4">My Favorite Movies</h2>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
       <div className="row">
         {favoriteMovies && favoriteMovies.map(movie => (
-          <div key={movie.id} className="col-lg-2 mb-4">
-            <MovieCard movie={movie} /> {/* Pass movie data to MovieCard component */}
-          </div>
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     </div>
